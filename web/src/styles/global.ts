39b7e57f0bd065f2588b2122c878f8e88c4f04d0
Gustavo Moreira <18315899+MGustav0@users.noreply.github.com>
2020-11-24 13:48:03 -0500

    Global styles aplicados

---
+++ web/src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body {
+    background: #342303;
+    color: #2D2D2D;
+  }
+
+  body, input, button {
+    font-family: Rubik, sans-serif;
+    font-size: 24px;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 700;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
